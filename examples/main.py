e435744c9af8144be189e9c4d2ccd329d7b814a7
dotX12 <hash@shitposting.team>
2022-04-20 17:24:58 +0300

    Fixes

--- examples/main.py
+++ examples/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from fastapi import File
 from fastapi import UploadFile
 
@@ -19,6 +19,18 @@ async def example_json_body_handler(
 
 
 @app.post("/form-data-body")
+async def example_foo_body_handler(
+    data: PostContractBodySchema = FormBody(),
+    document: UploadFile = File(...),
+):
+    return {
+        "title": data.title,
+        "date": data.date,
+        "file_name": document.filename
+    }
+
+
+@app.post("/form-data-body-two")
 async def example_foo_body_handler(
     data: PostContractBodySchema = FormBody(),
     document: UploadFile = File(...),
